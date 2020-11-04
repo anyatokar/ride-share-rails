ba0c7d793b22b3cb0d6457cbaecd7f19e3d71c4c
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-11-03 16:31:23 -0800

    added RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # TODO-- determine which RESTful routes are needed later
+  resources :drivers
+  resources :trips
+  resources :passengers
 end
