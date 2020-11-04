53035abb1d952e1a43904e5fd50f43ef8319ad42
Ren Carothers <rencarothers@gmail.com>
2020-11-04 15:47:37 -0800

    added basic show method and views for all three models

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,13 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all
   end
+
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,13 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def show
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
 end
