8c46c47cb0189215341a727ca3cddf71c5da6141
Ren Carothers <rencarothers@gmail.com>
2020-11-05 12:01:59 -0800

    updated show view for trips, added links to trip show in driver and pass tables, added basic edit for trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,4 +11,48 @@ class TripsController < ApplicationController
       return
     end
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trip_path(@trip)
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+      return
+    elsif @trip.update(trip_params)
+      redirect_to trip_path(@trip)
+      return
+    else
+    render :edit
+    return
+    end
+  end
+
+  private
+  def trip_params
+    return params.require(:trip).permit(:driver_id, :passenger_id, :cost, :rating) #TODO: should all params be required - yes otherwise edit won't work but what about a new trip that's being assigned a driver- shouldn't have all required?
+  end
 end
+
