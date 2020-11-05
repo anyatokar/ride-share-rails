84ba7d1c28d2a97f3ecf83d02e228a7c2c0c43c0
Ren Carothers <rencarothers@gmail.com>
2020-11-04 22:44:05 -0800

    added pagination to indices, created homepage as root_path

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,5 +1,6 @@
 class DriversController < ApplicationController
   def index
-    @drivers = Driver.all
+    # @drivers = Driver.all
+    @drivers = Driver.paginate(page: params[:page]).order("id")
   end
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,5 +1,6 @@
 class PassengersController < ApplicationController
   def index
-    @passengers = Passenger.all
+    # @passengers = Passenger.all
+    @passengers = Passenger.paginate(page: params[:page]).order("id")
   end
 end
