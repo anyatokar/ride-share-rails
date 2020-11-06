7396f191bc6e81a73afee7d853fa7858e06f2564
Ren Carothers <rencarothers@gmail.com>
2020-11-05 22:58:06 -0800

    adjusted currency methods to be useable by multiple methods if needed, updated driver index to display status vs true or false

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,16 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_spent
+    total = 0
+    self.trips.each do |trip|
+      total += trip.cost.to_i
+    end
+    return total
+  end
+
+  def to_currency(amount)
+    currency = amount.to_i/(100.0).round(2)
+    return currency
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,8 +2,8 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-  def to_currency
-    currency = (self.cost.to_i)/(100.0).round(2)
+  def to_currency(amount)
+    currency = amount.to_i/(100.0).round(2)
     return currency
   end
 end
